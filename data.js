525c477eac9c62c7e04c3bc2ee0c62b212b1fd72
yanuarprayoga09 <yanuarprayoga@gmail.com>
2023-06-18 17:12:59 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,9 @@
+
+const user = [
+    { nim: '220302072', nama: 'Yanuar Prayoga', divisi: 'Web' },
+    { nim: '220302073', nama: 'Rina Amelia', divisi: 'Mobile' },
+    { nim: '220302074', nama: 'Budi Santoso', divisi: 'Web' },
+    { nim: '220302075', nama: 'Dewi Indriani', divisi: 'Mobile' },
+    { nim: '220302076', nama: 'Ahmad Ridwan', divisi: 'Web' }
+    // Tambahkan data pengguna lainnya di sini
+  ];
---
+++ main.js
@@ -0,0 +1,23 @@
+const users = [
+    { nim: '220302072', nama: 'Yanuar Prayoga', divisi: 'Web' },
+    { nim: '220302073', nama: 'Rina Amelia', divisi: 'Mobile' },
+    { nim: '220302074', nama: 'Budi Santoso', divisi: 'Web' },
+    { nim: '220302075', nama: 'Dewi Indriani', divisi: 'Mobile' },
+    { nim: '220302076', nama: 'Ahmad Ridwan', divisi: 'Web' }
+    // Tambahkan data pengguna lainnya di sini
+  ];
+
+  function cekHasil() {
+    const nimInput = document.getElementById('nimInput').value;
+    const user = users.find(user => user.nim === nimInput);
+  
+    if (user) {
+      const message = `Selamat ${user.nama}, Anda diterima sebagai pengurus Protic di divisi ${user.divisi}.`;
+      const encodedMessage = encodeURIComponent(message);
+      window.location.href = `pengumuman.html?message=${encodedMessage}`; // Mengarahkan ke halaman pengumuman dengan pesan pengumuman sebagai parameter URL
+    } else {
+      const message = 'Maaf, NIM Anda tidak terdaftar sebagai pengurus Protic.';
+      const encodedMessage = encodeURIComponent(message);
+      window.location.href = `pengumuman.html?message=${encodedMessage}`; // Mengarahkan ke halaman pengumuman dengan pesan pengumuman sebagai parameter URL
+    }
+  }
